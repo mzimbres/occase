2b8972aed5e6f07ef5bc03c13caee45bcb508e98
Marcelo Zimbres <mzimbres@gmail.com>
2018-07-28 18:17:02 +0200

    Makes many with data encapsulation..

--- server.cpp
+++ server.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include <set>
 #include <stack>
 #include <vector>
 #include <algorithm>
@@ -8,21 +9,21 @@
 using uid_type = long long;
 using gid_type = long long;
 
-struct user {
+class user {
+private:
    // The operations we are suposed to perform on an user's friends
    // are
    //
-   // 1. Insert: Always in the back O(1).
-   // 2. Remove: Once in a while we may have to remove a friend. O(n).
-   // 3. Search. I do not think we will perfrórm read-only searches.
+   // 1. Insert: On registration and as user adds or remove friends.
+   // 2. Remove: On registration and as user adds or remove friends.
+   // 3. Search. I do not think we will perform read-only searches.
    //
    // Revove should happens with even less frequency that insertion,
-   // what makes this operation non-critical.  Another option would be
-   // to use a std::set but I want to avoid linked data structures for
-   // now. The number of friends is expected to be no more that 1000.
-   // If we begin to perform searche often, we may have to change this
-   // to a data structure with faster lookups.
-   std::vector<uid_type> friends;
+   // what makes this operation non-critical. The number of friends is
+   // expected to be no more that 1000.  If we begin to perform
+   // searche often, we may have to change this to a data structure
+   // with faster lookups.
+   std::set<uid_type> friends;
 
    // The user is expected to create groups, of which he becomes the
    // owner. The total number of groups a user creates won't be high
@@ -36,9 +37,15 @@ struct user {
    // A vector seems the most suitable for these requirements.
    std::vector<gid_type> own_groups;
 
-   // Removes group owned by this user from his list of groups.
-   void remove_owned_group(gid_type group)
+public:
+   void add_friend(uid_type uid)
    {
+      friends.insert(uid);
+   }
+
+   void remove_group(gid_type group)
+   {
+      // Removes group owned by this user from his list of groups.
       auto group_match =
          std::remove( std::begin(own_groups), std::end(own_groups)
                     , group);
@@ -48,8 +55,6 @@ struct user {
 
    void add_group(gid_type gid)
    {
-      // After seting we have to notify the new user he owns a new
-      // group.
       own_groups.push_back(gid);
    }
 
@@ -62,26 +67,38 @@ struct user {
    // messages to to group.
 };
 
-struct group {
+class group {
+private:
    uid_type owner {-1}; // The user that owns this group.
 
    // The number of members in a group is expected be be on the
-   // thousends, let us say 100k. The operations performed are
+   // thousands, let us say 100k. The operations performed are
    //
-   // 1. Insert: Quite often on the back resulting in O(1).
-   // 2. Remove: Once in a while
+   // 1. Insert: Quite often. To avoid inserting twice we have search
+   //            before inserting.
+   // 2. Remove: Once in a while. Also requires searching.
    // 3. Search: I am not sure yet, but for security reasons we may
-   //            have to always check if the user has right to
+   //            have to always check if the user has the right to
    //            announce in the group in case the app sends us an
-   //            incorect old group id.
+   //            incorrect old group id.
    // 
-   // For now I will stick with a vector that is very bad if 3. is
-   // true and change it later if the need arises.
+   // Given those requirements above, I think a hash table is more
+   // appropriate.
    std::vector<uid_type> members;
+
+public:
+   auto get_owner() const noexcept {return owner;}
+   void set_owner(uid_type uid) noexcept {owner = uid;}
 
    auto is_owned_by(uid_type uid) const noexcept
    {
-      return owner == uid;
+      return uid > 0 && owner == uid;
+   }
+
+   void reset()
+   {
+      owner = -1;
+      members = {};
    }
 };
 
@@ -97,10 +114,9 @@ struct server_data {
    std::stack<gid_type> avail_groups_idxs;
    std::vector<group> groups;
 
-   void add_user(uid_type id, user u)
+   void add_user(uid_type id, std::vector<uid_type> contacts)
    {
-      // Consider insert_or_assign and also std::move.
-      auto new_user = users.insert({id, u});
+      auto new_user = users.insert({id, {}});
       if (!new_user.second) {
          // The user already exists. This case can be triggered by
          // some conditions
@@ -108,34 +124,34 @@ struct server_data {
          //    and is sending us his new contacts.
          // 2. The user lost his phone and the number was assigned to
          //    a new person.
-         // REVIEW: For now we will simply override previous values
-         // and decide later how to handle (2) properly.
-
-         // REVIEW: Is it correct to assume that if the insertion
-         // failed than the user object remained unmoved? 
-         new_user.first->second.friends = std::move(u.friends);
-         return;
+         // REVIEW: For now we will handle 1. and think about 2.
+         // later.
+         //
+         // Some of the user contacts are not registered, that means
+         // we have to check one for one. But since this is not any
+         // different that in the case the user did not exist we can
+         // continue.
       }
 
-      // Insertion took place and now we have to test which of its
-      // friends are already registered and add them as the user
-      // friends.
-      for (auto const& o : u.friends) {
+      // Regardless whether insertion took place or not, we have to
+      // test which of its friends are already registered and add them
+      // as a user friend one by one.
+      for (auto const& o : contacts) {
          auto existing_user = users.find(o);
          if (existing_user == std::end(users))
             continue;
 
          // A contact was found in our database, let us add him as a
          // user friend. We do not have to check if it is already in
-         // the array as case would handled by the if above.
-         new_user.first->second.friends.push_back(o);
+         // the array as case would handled by the set automaticaly.
+         new_user.first->second.add_friend(o);
 
          // REVIEW: We also have to inform the existing user that one of
-         // his contacts registered. This will be made only upon
+         // his contacts just registered. This will be made only upon
          // request, for now, we will only add the new user in the
-         // existing user's friends and him be notified if the new
+         // existing user's friends and let him be notified if the new
          // user sends him a message.
-         existing_user->second.friends.push_back(new_user.first->first);
+         existing_user->second.add_friend(new_user.first->first);
       }
    }
 
@@ -154,8 +170,7 @@ struct server_data {
 
    void dealloc_group(gid_type gid)
    {
-      groups[gid].members = {};
-      groups[gid].owner = -1;
+      groups[gid].reset();
       avail_groups_idxs.push(gid);
    }
 
@@ -167,7 +182,7 @@ struct server_data {
       groups[gid] = g;
 
       // Updates the owner with his new group.
-      auto match = users.find(g.owner);
+      auto match = users.find(g.get_owner());
       if (match == std::end(users)) {
          // This is a non-existing user. Perhaps the json command was
          // sent with the wrong information signaling a logic error in
@@ -175,7 +190,9 @@ struct server_data {
          return static_cast<gid_type>(-1);
       }
 
-      match->second.own_groups.push_back(gid);
+      // After seting we have to notify the new user he owns a new
+      // group.
+      match->second.add_group(gid);
       return gid;
    }
 
@@ -184,34 +201,28 @@ struct server_data {
       return gid >= 0 && gid < groups.size();
    }
 
-   group remove_group(uid_type uid, gid_type gid)
+   group remove_group(uid_type owner, gid_type gid)
    {
       if (!gid_in_range(gid))
          return {}; // Out of range? Logic error.
 
       // The user must be the owner of the group to be allowed to
       // remove it
-      if (!groups[gid].is_owned_by(uid))
+      if (!groups[gid].is_owned_by(owner))
          return {}; // Sorry, you are not allowed.
 
-      if (groups[gid].owner == -1) {
-         // This group is already deallocated. The caller called us
-         // with the wrong gid.
-         return {};
-      }
-
-      // To remove a user we have to inform its members the group
-      // has been removed, therefore we will make a copy before
-      // removing and return it.
+      // To remove a group we have to inform its members the group has
+      // been removed, therefore we will make a copy before removing
+      // and return it.
       auto removed_group = groups[gid];
       dealloc_group(gid);
 
       // Now we have to remove this gid from the owners list.
-      auto user_match = users.find(removed_group.owner);
+      auto user_match = users.find(removed_group.get_owner());
       if (user_match == std::end(users))
          return {}; // This looks like an internal logic problem.
 
-      user_match->second.remove_owned_group(gid);
+      user_match->second.remove_group(gid);
       return removed_group;
    }
 
@@ -233,9 +244,9 @@ struct server_data {
          return false;
 
       // The new owner exists.
-      groups[gid].owner = to;
+      groups[gid].set_owner(to);
       to_match->second.add_group(gid);
-      from_match->second.remove_owned_group(gid);
+      from_match->second.remove_group(gid);
       return true;
    }
 };

dc18d4df33adecdf6acbdef571d578713c90f2d8
Marcelo Zimbres <mzimbres@gmail.com>
2018-07-29 21:53:34 +0200

    Adds groups visibility and membership info.

--- server.cpp
+++ server.cpp
@@ -2,6 +2,7 @@
 
 #include <set>
 #include <stack>
+#include <chrono>
 #include <vector>
 #include <algorithm>
 #include <unordered_map>
@@ -73,16 +74,49 @@ public:
    // messages to to group.
 };
 
+enum class group_membership
+{
+   // Member is allowed to read posts and has no access to poster
+   // contact. That means in practice he cannot buy anything but only
+   // see the traffic in the group and decide whether it is worth to
+   // updgrade.
+   WATCH
+
+   // Allowed to see posts, see poster contact and post. He gets posts
+   // with delay of some hours and his posts are subject to whether he
+   // has provided the bank details.
+,  DEFAULT
+   
+   // Like DEFAULT but get posts without any delay.
+,  PREMIUM
+};
+
+struct group_mem_info {
+   group_membership membership {group_membership::DEFAULT};
+   std::chrono::seconds delay {0};
+};
+
+enum class group_visibility
+{
+   // Members do not need auhorization to enter the group.
+   PUBLIC
+
+   // Only the owner can add members.
+,  PRIVATE
+};
+
 class group {
 private:
+   group_visibility visibility {group_visibility::PUBLIC};
+   
    uid_type owner {-1}; // The user that owns this group.
 
-   // The number of members in a group is expected be be on the
+   // The number of members in a group is expected be on the
    // thousands, let us say 100k. The operations performed are
    //
    // 1. Insert: Quite often. To avoid inserting twice we have search
    //            before inserting.
-   // 2. Remove: Once in a while. Also requires searching.
+   // 2. Remove: Once in a while, also requires searching.
    // 3. Search: I am not sure yet, but for security reasons we may
    //            have to always check if the user has the right to
    //            announce in the group in case the app sends us an
@@ -90,7 +124,8 @@ private:
    // 
    // Given those requirements above, I think a hash table is more
    // appropriate.
-   std::unordered_set<uid_type> members;
+   std::unordered_map< uid_type
+                     , group_mem_info> members;
 
 public:
    auto get_owner() const noexcept {return owner;}
@@ -109,7 +144,7 @@ public:
 
    void add_member(uid_type uid)
    {
-      members.insert(uid);
+      members.insert({uid, {}});
    }
 
    void remove_member(uid_type uid)

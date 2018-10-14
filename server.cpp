0386fcaece1b68f3c1836ce76fc53f3d09a170e4
Marcelo Zimbres <mzimbres@gmail.com>
2018-10-14 21:43:35 +0200

    Moves the parsing of command line options to a function.

--- server.cpp
+++ server.cpp
@@ -29,6 +29,7 @@ struct signal_handler {
 namespace po = boost::program_options;
 
 struct server_op {
+   bool help = false;
    std::string ip;
    unsigned short port;
    int auth_timeout;
@@ -49,56 +50,65 @@ struct server_op {
    }
 };
 
+server_op get_server_op(int argc, char* argv[])
+{
+   server_op op;
+   po::options_description desc("Options");
+   desc.add_options()
+   ("help,h", "Produces help message")
+   ( "port,p"
+   , po::value<unsigned short>(&op.port)->default_value(8080)
+   , "Server listening port."
+   )
+   ("ip,d"
+   , po::value<std::string>(&op.ip)->default_value("127.0.0.1")
+   , "Server ip address."
+   )
+   ("sms-timeout,s"
+   , po::value<int>(&op.sms_timeout)->default_value(2)
+   , "SMS confirmation timeout in seconds."
+   )
+   ("auth-timeout,a"
+   , po::value<int>(&op.auth_timeout)->default_value(2)
+   , "Authetication timeout in seconds. Fired after the websocket "
+     "handshake completes. Used also by the login "
+     "command for clients registering for the first time."
+   )
+   ("handshake-timeout,k"
+   , po::value<int>(&op.handshake_timeout)->default_value(2)
+   , "Handshake timeout in seconds. If the websocket handshake lasts "
+     "more than that the socket is shutdown and closed."
+   )
+   ("pong-timeout,r"
+   , po::value<int>(&op.pong_timeout)->default_value(2)
+   , "Pong timeout in seconds. This is the time the client has to "
+     "reply a ping frame sent by the server. If a pong is received "
+     "on time a new ping is sent on timer expiration."
+   )
+   ("close-frame-timeout,e"
+   , po::value<int>(&op.close_frame_timeout)->default_value(2)
+   , "The time we are willing to wait for a reply of a sent close frame."
+   )
+   ;
+
+   po::variables_map vm;        
+   po::store(po::parse_command_line(argc, argv, desc), vm);
+   po::notify(vm);    
+
+   if (vm.count("help")) {
+      std::cout << desc << "\n";
+      op.help = true;
+   }
+
+   return op;
+}
+
 int main(int argc, char* argv[])
 {
    try {
-      server_op op;
-      po::options_description desc("Options");
-      desc.add_options()
-      ("help,h", "Produces help message")
-      ( "port,p"
-      , po::value<unsigned short>(&op.port)->default_value(8080)
-      , "Server listening port."
-      )
-      ("ip,d"
-      , po::value<std::string>(&op.ip)->default_value("127.0.0.1")
-      , "Server ip address."
-      )
-      ("sms-timeout,s"
-      , po::value<int>(&op.sms_timeout)->default_value(2)
-      , "SMS confirmation timeout in seconds."
-      )
-      ("auth-timeout,a"
-      , po::value<int>(&op.auth_timeout)->default_value(2)
-      , "Authetication timeout in seconds. Fired after the websocket "
-        "handshake completes. Used also by the login "
-        "command for clients registering for the first time."
-      )
-      ("handshake-timeout,k"
-      , po::value<int>(&op.handshake_timeout)->default_value(2)
-      , "Handshake timeout in seconds. If the websocket handshake lasts "
-        "more than that the socket is shutdown and closed."
-      )
-      ("pong-timeout,r"
-      , po::value<int>(&op.pong_timeout)->default_value(2)
-      , "Pong timeout in seconds. This is the time the client has to "
-        "reply a ping frame sent by the server. If a pong is received "
-        "on time a new ping is sent on timer expiration."
-      )
-      ("close-frame-timeout,e"
-      , po::value<int>(&op.close_frame_timeout)->default_value(2)
-      , "The time we are willing to wait for a reply of a sent close frame."
-      )
-      ;
-
-      po::variables_map vm;        
-      po::store(po::parse_command_line(argc, argv, desc), vm);
-      po::notify(vm);    
-
-      if (vm.count("help")) {
-         std::cout << desc << "\n";
+      auto const op = get_server_op(argc, argv);
+      if (op.help)
          return 0;
-      }
 
       auto const address = boost::asio::ip::make_address(op.ip);
 
@@ -117,7 +127,7 @@ int main(int argc, char* argv[])
 
       ioc.run();
       return 0;
-   } catch(std::exception const& e) {
+   } catch (std::exception const& e) {
        std::cerr << "error: " << e.what() << "\n";
        return 1;
    }

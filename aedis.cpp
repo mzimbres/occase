7a4d419aa389242d3fe4d685ca84197f13a5992c
Marcelo Zimbres <mzimbres@gmail.com>
2018-11-13 21:42:08 +0100

    First steps debugging redis session.

--- aedis.cpp
+++ aedis.cpp
@@ -17,20 +17,74 @@ using namespace rt;
 
 namespace po = boost::program_options;
 
+void pub(redis_session_cf const& cf)
+{
+   auto const pub_handler = []( auto const& ec
+                          , auto const& data
+                          , auto const& cmd)
+   {
+        if (ec)
+           throw std::runtime_error(ec.message());
+
+        //for (auto const& o : data)
+        //   std::cout << o << " ";
+        //
+        //std::cout << std::endl;
+   };
+
+   boost::asio::io_context ioc;
+   redis_session pub_session(cf, ioc);
+   pub_session.set_on_msg_handler(pub_handler);
+   for (auto i = 0; i < 10000; ++i)
+      pub_session.send(gen_resp_cmd( redis_cmd::publish
+                                   , {"foo", std::to_string(i)}));
+   pub_session.run();
+
+   ioc.run();
+}
+
+void sub(redis_session_cf const& cf)
+{
+   auto const sub_handler = []( auto const& ec
+                              , auto const& data
+                              , auto const& cmd)
+   {
+        if (ec) 
+           throw std::runtime_error(ec.message());
+
+        for (auto const& o : data)
+           std::cout << o << " ";
+        
+        std::cout << std::endl;
+   };
+
+   boost::asio::io_context ioc;
+   redis_session sub_session(cf, ioc);
+   sub_session.set_on_msg_handler(sub_handler);
+   sub_session.send(gen_resp_cmd(redis_cmd::subscribe, {"foo"}));
+   sub_session.run();
+   ioc.run();
+}
+
 int main(int argc, char* argv[])
 {
    try {
+      auto test = 0;
       redis_session_cf cf;
       po::options_description desc("Options");
       desc.add_options()
       ("help,h", "Produces help message")
       ( "port,p"
       , po::value<std::string>(&cf.port)->default_value("6379")
-      , "Server listening port."
+      , "Redis server listening port."
       )
       ("host,i"
       , po::value<std::string>(&cf.host)->default_value("127.0.0.1")
-      , "Server ip address."
+      , "Redis server ip address."
+      )
+      ("Run mode,t"
+      , po::value<int>(&test)->default_value(-1)
+      , "1 for pub, 2 for sub."
       )
       ;
 
@@ -43,52 +97,33 @@ int main(int argc, char* argv[])
          return 0;
       }
 
-      boost::asio::io_context ioc;
-      redis_session sub_session(cf, ioc);
+      if (test == 1) {
+         pub(cf);
+         return 0;
+      }
 
-      auto const handler = []( auto const& ec
-                             , auto const& data
-                             , auto const& cmd)
-      {
-           if (ec) {
-              std::cout << ec.message() << std::endl;
-              return;
-           }
+      if (test == 2) {
+         sub(cf);
+         return 0;
+      }
 
-           for (auto const& o : data)
-              std::cout << o << " ";
-           
-           std::cout << std::endl;
-      };
+      //pub_session.send(gen_resp_cmd(redis_cmd::incrby, {"foo", "3"}));
+      //pub_session.send(gen_resp_cmd(redis_cmd::get, {"foo"}));
+      //pub_session.send(gen_resp_cmd( redis_cmd::ping
+      //                             , {"Arbitrary message."}));
+      //pub_session.send(gen_resp_cmd( redis_cmd::publish
+      //                             , {"foo", "Message."}));
 
-      sub_session.set_on_msg_handler(handler);
-      sub_session.send(gen_resp_cmd(redis_cmd::subscribe, {"foo"}));
-      sub_session.run();
-
-      redis_session pub_session(cf, ioc);
-      pub_session.set_on_msg_handler(handler);
-
-      pub_session.send(gen_resp_cmd(redis_cmd::set, {"foo", "20"}));
-      pub_session.send(gen_resp_cmd(redis_cmd::incrby, {"foo", "3"}));
-      pub_session.send(gen_resp_cmd(redis_cmd::get, {"foo"}));
-      pub_session.send(gen_resp_cmd( redis_cmd::ping
-                                   , {"Arbitrary message."}));
-      for (auto i = 0; i < 2; ++i)
-         pub_session.send(gen_resp_cmd( redis_cmd::publish
-                                      , {"foo", "Message."}));
-
-      pub_session.send(gen_resp_cmd( redis_cmd::ping
-                                   , {"Arbitrary message2."}));
-      pub_session.send(gen_resp_cmd( redis_cmd::lpop
-                                   , {"nonsense"}));
-      pub_session.send(gen_resp_cmd( redis_cmd::ping
-                                   , {"Arbitrary message3."}));
-      pub_session.send(gen_resp_cmd( redis_cmd::rpush
-                                   , {"nonsense", "one", "two", "three"}));
-      pub_session.send(gen_resp_cmd( redis_cmd::lrange
-                                   , {"nonsense", "0", "-1"}));
-      pub_session.run();
-      ioc.run();
+      //pub_session.send(gen_resp_cmd( redis_cmd::ping
+      //                             , {"Arbitrary message2."}));
+      //pub_session.send(gen_resp_cmd( redis_cmd::lpop
+      //                             , {"nonsense"}));
+      //pub_session.send(gen_resp_cmd( redis_cmd::ping
+      //                             , {"Arbitrary message3."}));
+      //pub_session.send(gen_resp_cmd( redis_cmd::rpush
+      //                             , {"nonsense", "one", "two", "three"}));
+      //pub_session.send(gen_resp_cmd( redis_cmd::lrange
+      //                             , {"nonsense", "0", "-1"}));
    } catch (std::exception& e) {
       std::cerr << "Exception: " << e.what() << "\n";
    }

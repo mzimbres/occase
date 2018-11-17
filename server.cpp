613ffc60bcc0a10d26f72757ab16db99afbffe68
Marcelo Zimbres <mzimbres@gmail.com>
2018-11-17 21:59:22 +0100

    Improves command line options.

--- server.cpp
+++ server.cpp
@@ -18,16 +18,17 @@ using namespace rt;
 namespace po = boost::program_options;
 
 struct config {
+   bool help = false;
    server_mgr_cf mgr;
-   std::vector<listener_cf> lts;
+   int workers;
+   std::vector<listener_cf> lts_cf;
 };
 
 auto get_server_op(int argc, char* argv[])
 {
-   int instances = 1;
    unsigned short port;
    std::string ip;
-   config op;
+   config cf;
    po::options_description desc("Options");
    desc.add_options()
    ("help,h", "Produces help message")
@@ -39,57 +40,57 @@ auto get_server_op(int argc, char* argv[])
    , po::value<std::string>(&ip)->default_value("127.0.0.1")
    , "Server ip address."
    )
-   ("instances"
-   , po::value<int>(&instances)->default_value(1)
-   , "The number of share-nothing server instances, each"
-     " one consuming one thread and having its own io_context"
-     " object. They will continuously occupy the ports"
-     " begining at --port."
+   ("workers,w"
+   , po::value<int>(&cf.workers)->default_value(1)
+   , "The number of worker threads, each"
+     " one consuming one thread and having its own io_context."
+     " For example, in a CPU with 8 cores this number should lie"
+     " around 5."
    )
    ("code-timeout,s"
-   , po::value<int>(&op.mgr.code_timeout)->default_value(2)
+   , po::value<int>(&cf.mgr.code_timeout)->default_value(2)
    , "Code confirmation timeout in seconds."
    )
    ("auth-timeout,a"
-   , po::value<int>(&op.mgr.auth_timeout)->default_value(2)
+   , po::value<int>(&cf.mgr.auth_timeout)->default_value(2)
    , "Authetication timeout in seconds. Fired after the websocket "
      "handshake completes. Used also by the login "
      "command for clients registering for the first time."
    )
    ("handshake-timeout,k"
-   , po::value<int>(&op.mgr.handshake_timeout)->default_value(2)
+   , po::value<int>(&cf.mgr.handshake_timeout)->default_value(2)
    , "Handshake timeout in seconds. If the websocket handshake lasts "
      "more than that the socket is shutdown and closed."
    )
    ("pong-timeout,r"
-   , po::value<int>(&op.mgr.pong_timeout)->default_value(2)
+   , po::value<int>(&cf.mgr.pong_timeout)->default_value(2)
    , "Pong timeout in seconds. This is the time the client has to "
      "reply a ping frame sent by the server. If a pong is received "
      "on time a new ping is sent on timer expiration."
    )
    ("close-frame-timeout,e"
-   , po::value<int>(&op.mgr.close_frame_timeout)->default_value(2)
+   , po::value<int>(&cf.mgr.close_frame_timeout)->default_value(2)
    , "The time we are willing to wait for a reply of a sent close frame."
    )
 
    ("redis-address"
-   , po::value<std::string>(&op.mgr.redis_address)->default_value("127.0.0.1")
+   , po::value<std::string>(&cf.mgr.redis_address)->default_value("127.0.0.1")
    , "Address of redis server."
    )
    ("redis-port"
-   , po::value<std::string>(&op.mgr.redis_port)->default_value("6379")
+   , po::value<std::string>(&cf.mgr.redis_port)->default_value("6379")
    , "Port where redis server is listening."
    )
    ("redis-group-channel"
-   , po::value<std::string>(&op.mgr.redis_group_channel)->default_value("group_msgs")
+   , po::value<std::string>(&cf.mgr.redis_group_channel)->default_value("group_msgs")
    , "The name of the redis channel where group messages will be broadcasted."
    )
    ("redis-menu-key"
-   , po::value<std::string>(&op.mgr.redis_menu_key)->default_value("menu")
+   , po::value<std::string>(&cf.mgr.redis_menu_key)->default_value("menu")
    , "Redis key holding the menu."
    )
    ("redis-msg-prefix"
-   , po::value<std::string>(&op.mgr.redis_msg_prefix)->default_value("msg")
+   , po::value<std::string>(&cf.mgr.redis_msg_prefix)->default_value("msg")
    , "That prefix that will be incorporated in the keys that hold"
      " user messages."
    )
@@ -101,16 +102,13 @@ auto get_server_op(int argc, char* argv[])
 
    if (vm.count("help")) {
       std::cout << desc << "\n";
-      return config {};
+      return config {true};
    }
 
-   std::vector<listener_cf> ops;
-   for (auto i = 0; i < instances; ++i) {
-      auto const p = static_cast<unsigned short>(port + i);
-      op.lts.push_back({ip, p});
-   }
+   cf.lts_cf.push_back({ip, port});
+   //cf.lts_cf.port = static_cast<unsigned short>(port);
 
-   return op;
+   return cf;
 }
 
 struct acceptor_pool {
@@ -144,18 +142,17 @@ struct acceptor_pool {
 int main(int argc, char* argv[])
 {
    try {
-      auto const op = get_server_op(argc, argv);
-      if (std::empty(op.lts))
+      auto const cf = get_server_op(argc, argv);
+      if (cf.help)
          return 0;
 
-      auto const generator = [mgr = op.mgr]()
+      auto const generator = [mgr = cf.mgr]()
       { return std::make_unique<mgr_arena>(mgr); };
 
       std::vector<std::unique_ptr<mgr_arena>> arenas;
-      std::generate_n(std::back_inserter(arenas), std::size(op.lts)
-                     , generator);
+      std::generate_n(std::back_inserter(arenas), cf.workers, generator);
 
-      acceptor_pool acc_pool {op.lts, arenas};
+      acceptor_pool acc_pool {cf.lts_cf, arenas};
       acc_pool.run();
 
       for (auto& o : arenas)

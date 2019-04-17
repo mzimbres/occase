a0de9e9ee3185483c20fa71f31416a95ee21565d
Marcelo Zimbres <mzimbres@gmail.com>
2019-04-17 16:03:28 +0200

    Adds comment.

--- src/run_all_tests.sh
+++ src/run_all_tests.sh
@@ -12,7 +12,10 @@ server_pid=$!
 # tests.
 sleep 3
 
+# This test must come first as it requires the client to receive a
+# specific number of messages.
 ./publish_tests --test 1 --publishers 300 --listeners 20
+
 ./publish_tests --test 3 --launch-interval 10
 ./publish_tests --test 2 --handshake-timeout 3
 ./publish_tests --test 4

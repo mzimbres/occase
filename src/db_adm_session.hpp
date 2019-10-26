326bbeb1dee4f748ccf286f35a9b49b5e04c4a48
Marcelo Zimbres <mzimbres@gmail.com>
2019-10-26 17:32:47 +0200

    Improvements.

--- src/db_adm_session.hpp
+++ src/db_adm_session.hpp
@@ -2,7 +2,7 @@
 
 #include <memory>
 #include <chrono>
-#include <memory>
+#include <algorithm>
 
 #include "net.hpp"
 #include "post.hpp"
@@ -11,30 +11,95 @@
 namespace rt
 {
 
-auto make_adm_page(std::vector<post> const& posts)
+namespace html
 {
-   json j;
-   j["items"] = posts;
+
+auto make_img(std::string const& img_host, std::string const& name)
+{
+   std::string img;
+   img += "<img src=\"";
+   img += img_host;
+   img += name;
+   img += "\">";
+   return img;
+}
+
+auto
+make_del_post_link( std::string const& img_host
+                  , std::string const& id)
+{
+   std::string del;
+   del += "<td>";
+   del += "<a href=\"";
+   del += img_host;
+   del += "/delete/id";
+   del += "\">Delete</a>";
+   del += "</td>";
+   return del;
+}
+
+auto make_img_row( std::string const& img_host
+                 , post const& p) noexcept
+{
+   try {
+      auto const j = json::parse(p.body);
+      auto const images = j.at("images").get<std::vector<std::string>>();
+      std::string row;
+      row += "<tr>";
+      row += make_del_post_link(img_host, "1234");
+      for (auto const& s : images) {
+         row += "<td>";
+         row += make_img(img_host, s);
+         row += "</td>";
+      }
+      row += "</tr>";
+      return row;
+   } catch (...) {
+   }
+
+   return std::string{};
+}
+
+auto make_adm_page( std::string const& img_host
+                  , std::vector<post> const& posts)
+{
+   // This is kind of weird, the database itself does not need to know
+   // anything about the body of the post. But to monitor what users
+   // post we have to parse the images here.
+
+   auto acc = [&](auto init, auto const& p)
+   {
+      init += make_img_row(img_host, p);
+      return init;
+   };
+
+   auto const rows =
+      std::accumulate( std::begin(posts)
+                     , std::end(posts)
+                     , std::string{}
+                     , acc);
+
+   std::string table;
+   table += "<table style=\"width:100%\">";
+   table += rows;
+   table += "</table>";
 
    std::string page;
-   page += "<!DOCTYPE html>\n";
-   page += "<html>\n";
-   page += "<body>\n";
-   page += "\n";
+   page += "<!DOCTYPE html>";
+   page += "<html>";
+   page += "<body>";
    page += "<h1>Occase administration pannel</h1>\n";
-   page += "\n";
-   page += "<p>Posts.</p>\n";
-   page += "\n";
-   page += j.dump();
+   page += "<p>Posts</p>";
+   page += table;
    page += "\n";
    page += "<a href=\"http://127.0.0.1:9091/delete\">This is a link</a>";
-   page += "</body>\n";
-   page += "</html>\n";
+   page += "</body>";
+   page += "</html>";
 
    return page;
 }
 
-auto post_del_ok()
+auto make_post_del_ok()
 {
    std::string page;
    page += "<!DOCTYPE html>\n";
@@ -47,6 +112,8 @@ auto post_del_ok()
    page += "</html>\n";
 
    return page;
+}
+
 }
 
 template <class Session>
@@ -135,14 +202,15 @@ private:
          log(loglevel::debug, "Posts have requested.");
          auto const posts = worker_.get_posts(0);
          response_.set(http::field::content_type, "text/html");
+         std::string const img_host = "http://occase.de:444/";
          boost::beast::ostream(response_.body())
-         << make_adm_page(posts);
+         << html::make_adm_page(img_host, posts);
       } else if (request_.target() == "/delete") {
          log(loglevel::info, "Post deletion.");
          // TODO: Check credentials to allow post deletion.
          response_.set(http::field::content_type, "text/html");
          boost::beast::ostream(response_.body())
-         << post_del_ok();
+         << html::make_post_del_ok();
       } else {
          response_.result(http::status::not_found);
          response_.set(http::field::content_type, "text/plain");
